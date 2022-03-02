53fe996a77ec9fe3c79a7143ab7f5f0d5faad4b2
dayznight7 <dayznight7@gmail.com>
2022-03-02 16:40:39 +0900

    realtime db test

---
+++ js/firebase_config.js
@@ -0,0 +1,25 @@
+/*
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-functions.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-storage.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-analytics.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-remote-config.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-performance.js"></script> 
+*/
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBp135SRE2yhsJNhuKfv0pqrsjufyXDO6U",
+  authDomain: "webtalk-eaea1.firebaseapp.com",
+  databaseURL: "https://webtalk-eaea1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "webtalk-eaea1",
+  storageBucket: "webtalk-eaea1.appspot.com",
+  messagingSenderId: "99195702290",
+  appId: "1:99195702290:web:871abd0d1442712a651ccb",
+  measurementId: "G-R6C2VTB5P5"
+};
+firebase.initializeApp(firebaseConfig);
